cc8c54f72a8587d87131376a4af91cc100cc81f6
lan-e <neladuranec@gmail.com>
2025-10-20 22:15:16 +0200

    set root to /uninwalk-api

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.api import router
 
 
-app = FastAPI()
+app = FastAPI(root_path="/uninwalk-api")
 app.include_router(router)
 
 origins = [
@@ -22,3 +23,6 @@ app.add_middleware(
 @app.get("/health")
 async def health_check():
     return {"status": "ok"}
+
+if __name__ == "__main__":
+    uvicorn.run(app=app, host="0.0.0.0", port=8000)
